c408b34f0693f3b08d6220dffed69968794fa5c6
Gan-Xing <599153574@qq.com>
2023-09-26 15:27:31 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="increment">Click me: {{ count }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, computed } from '@vue/composition-api';
+import { Component } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const count = ref(0);
+    const message = computed(() => `Hello Vite Vue2 with Composition API!`);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      message,
+      increment
+    };
+  }
+} as Component;
+</script>
